f77e9fef4c471ca6ea9b69a98334b0575df9c6cd
Unfabul <yoteam@yandex.ru>
2019-06-08 12:02:38 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,54 @@
+let slider = document.querySelector('.banner-slider');
+// let sliderSlide = document.querySelectorAll('.slide');
+let sliderBullets = document.querySelectorAll('.bullet');
+
+
+// let i = 1;
+// setInterval(function() {
+//     if(i>5){
+//         i = 1;
+//         slider.style.transform = 'translate(-'+ window.innerWidth*i + 'px)';
+//         i = 2;
+//     }else{
+//         slider.style.transform = 'translate(-'+ window.innerWidth*i + 'px)';
+//         i++;
+//     }
+// }, 1000);
+
+// Слайдер
+sliderBullets.forEach(function(sliderBulletsEl, index){
+
+    sliderBulletsEl.addEventListener('click', function(){
+        sliderBullets.forEach(function(sliderBulletsActiveEl, index){
+            sliderBulletsActiveEl.classList.remove('active');
+        });
+        this.classList.add('active');
+        slider.style.transform = 'translate(-'+ document.body.clientWidth*index + 'px)';
+        console.log(slider.style.transform);
+    });
+
+});
+
+let tours = document.querySelector('.tours-line'),
+    tour = document.querySelector('.tour');
+tours.addEventListener('click', function(){
+    if(tours.classList.contains('open')){
+        tour.style.display = 'block';
+        tours.classList.remove('open');
+    }else{
+        tour.style.display = 'none';
+        tours.classList.add('open');
+    }
+});
+
+let servs = document.querySelector('.serv-line'),
+    serv = document.querySelector('.serv');
+servs.addEventListener('click', function(){
+    if(servs.classList.contains('open')){
+        serv.style.display = 'block';
+        servs.classList.remove('open');
+    }else{
+        serv.style.display = 'none';
+        servs.classList.add('open');
+    }
+});
